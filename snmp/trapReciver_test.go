1b43f5738517652281a3880fc8424ce7e0be97f7
Smark <smark@freecoop.net>
2019-06-18 00:33:30 +0800

    update

--- snmp/trapReciver_test.go
+++ snmp/trapReciver_test.go
@@ -2,12 +2,11 @@ package snmp
 
 import (
 	"testing"
-	"time"
 )
 
 func TestTrapReciver_OnNewTrap(t *testing.T) {
 	r := &TrapReciver{Address: "172.16.2.2"}
 	defer r.Close()
 	r.Start()
-	time.Sleep(30 * time.Second)
+	//time.Sleep(30 * time.Second)
 }
